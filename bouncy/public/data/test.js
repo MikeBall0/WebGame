7601c4752f2f81f263c78eb154979a19e3d2ffda
MikeBall0 <mike.ball@mikutech.com>
2015-02-24 19:58:49 -0500

    removed unneccessary file

--- bouncy/public/data/test.js
+++
@@ -1,4 +0,0 @@
-var top = Math.min(this.top(), other.top());
-var top = Math.min(this.top(), other.top());
-var top = Math.min(this.top(), other.top());
-var top = Math.min(this.top(), other.top());
